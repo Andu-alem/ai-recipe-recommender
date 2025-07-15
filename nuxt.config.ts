3e4ecfb42cfe2abfd44104d0b815e8521e46374d
Andualem <andualem.fereja12@gmail.com>
2025-07-15 17:04:16 +0300

    feat: add shadcn components and configure mongodb connection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxt-mongoose']
+  modules: [
+    '@nuxt/eslint',
+    'nuxt-mongoose',
+    'shadcn-nuxt',
+    '@vee-validate/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/devtools',
+    '@pinia/nuxt'
+  ],
+  mongoose: {
+    uri: process.env.MONGODB_URI,
+    options: {},
+    modelsDir: 'models',
+    devtools: true
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
