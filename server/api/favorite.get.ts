6ee9c83692ff2ab76feb9f7858d545b43e7147a3
Andualem <andualem.fereja12@gmail.com>
2025-08-05 15:32:25 +0300

    feat: create an endpoint to fetch saved recipes

---
+++ server/api/favorite.get.ts
@@ -0,0 +1,19 @@
+import { auth } from "~/lib/auth"
+
+export default defineEventHandler(async (event) => {
+    const session = await auth.api.getSession({
+        headers: event.headers
+    })
+    try {
+        return await RecipeSchema.findOne({
+            user: session?.session.userId
+        })
+    }
+    catch {
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'not found!',
+        })
+    }
+})
+  
