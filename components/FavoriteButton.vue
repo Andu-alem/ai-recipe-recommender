9750eac4e03885698312cb495c87e5e04510b452
Andualem <andualem.fereja12@gmail.com>
2025-08-05 15:20:15 +0300

    feat: create FavoriteButton component to allow saving of favorite

---
+++ components/FavoriteButton.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { GeneratedRecipe } from '../types'
+import { usePreferencesStore } from '~/stores/preferences'
+
+const isSaved = ref(false)
+const isSaving = ref(false)
+
+const props = defineProps<{
+    recipe: GeneratedRecipe,
+}>()
+
+const { preferences } = usePreferencesStore()
+
+async function saveRecipe() {
+    isSaving.value = true
+    try {
+        await $fetch('/api/favorite', {
+            method: 'POST',
+            body: {
+                ...preferences,
+                userIngredients: preferences?.ingredients ?? '',
+                ...props.recipe
+            }
+        })
+        isSaved.value = true
+    } catch {
+        // make a toast here to notify a user about the error
+        console.log("Error has occured here, do something")
+    } finally {
+        isSaving.value = false
+    }
+}
+
+</script>
+
+<template>
+    <Button
+        variant="ghost"
+        size="sm"
+        class=" bg-white/80 hover:bg-white rounded-full p-2"
+        @click="saveRecipe"
+    >
+        <LucideHeart
+            class="w-4 h-4"
+            :class="{
+                'fill-red-500 text-red-500': isSaved,
+                'text-stone-600': false,
+                'animate-pulse': isSaving
+            }"
+        />
+    </Button>
+</template>
