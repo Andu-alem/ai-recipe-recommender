779aa56c3f881fb93d9a4c045c4a4bb00459e54e
Andualem <andualem.fereja12@gmail.com>
2025-08-07 19:47:15 +0300

    feat: add theme toggler functionality

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,13 +3,16 @@
     <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-            <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4 bg-white">
+            <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
                 <SidebarTrigger class="-ml-1" />
-                <div class="flex items-center space-x-2">
-                    <div class="flex aspect-square size-8 items-center justify-center rounded-full bg-emerald-500 text-sidebar-primary-foreground">
-                        <span class="text-lg">🥘</span>
+                <div class="flex-1 flex justify-between items-center">
+                    <div class="flex gap-2 itemx-center">
+                        <div class="flex aspect-square size-8 items-center justify-center rounded-full bg-emerald-500 text-sidebar-primary-foreground">
+                            <span class="text-lg">🥘</span>
+                        </div>
+                        <span class="font-semibold text-emerald-800">ChefGPT</span>
                     </div>
-                    <span class="font-semibold text-emerald-800">ChefGPT</span>
+                    <ThemeToggler />
                 </div>
             </header>
             <main class="flex-1">
