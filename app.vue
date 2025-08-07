0727ccf2db8f4594bdd8a5511ee5db1f3166aac0
Andualem <andualem.fereja12@gmail.com>
2025-08-07 19:43:49 +0300

    style: add style import for toaster component

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import 'vue-sonner/style.css'
+</script>
+
 <template>
     <NuxtLayout>
       <NuxtRouteAnnouncer />
       <NuxtPage />
-      <Toaster position="top-right" rich-colors />
+      <Toaster position="bottom-right" rich-colors />
     </NuxtLayout>
 </template>
