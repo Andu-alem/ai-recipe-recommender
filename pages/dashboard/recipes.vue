d0891cb774b04c001b9909b736f6fb5c6eb4d42a
Andualem <andualem.fereja12@gmail.com>
2025-08-07 23:15:21 +0300

    refactor: create a dedicated recipe card component for recipes

--- pages/dashboard/recipes.vue
+++ pages/dashboard/recipes.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { usePreferencesStore } from '~/stores/preferences'
-import type { GeneratedRecipe } from '~/types'
+import type { GeneratedRecipe, BaseRecipe } from '~/types'
+
+const recipes = ref<BaseRecipe[]>()
 
 definePageMeta({
     layout: "dashboard"
@@ -12,6 +14,15 @@ const { data, pending, error, refresh } = await useFetch<GeneratedRecipe[]>('/ap
     method: 'POST',
     body: JSON.stringify(preference)
 })
+
+if (data && preference) {
+    recipes.value = data.value.map(recipe => ({
+        ...preference,
+        ...recipe,
+        ingredients: recipe.tags,
+        userIngredients: preference.ingredients
+    }))
+}
 
 </script>
 
@@ -35,46 +46,7 @@ const { data, pending, error, refresh } = await useFetch<GeneratedRecipe[]>('/ap
                 </div>
             </div>
             <div v-if="data" class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-                <Card
-                    v-for="recipe in data"
-                    :key="recipe.name"
-                    class="shadow-lg gap-0 cursor-pointer hover:shadow-xl transition-shadow"
-                >
-                    <CardHeader>
-                        <CardTitle class="flex justify-between items-center px-2">
-                            <h3 class="font-semibold">{{recipe.name}}</h3>
-                            <FavoriteButton :recipe />
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <Separator />
-                        <div class="text-sm space-y-1">
-                            <h3 class="text-primary font-semibold">Instructions</h3>
-                            <div v-for="instruction, index in recipe.instructions" :key="instruction">
-                                {{ index+1 }} - {{ instruction }}
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap items-center gap-2 text-sm text-foreground/85 my-4">
-                            <div class="flex items-center space-x-1">
-                                <LucideClock class="w-4 h-4" />
-                                <span>{{recipe.cookTime}}</span>
-                            </div>
-                            <div class="flex items-center space-x-1">
-                                <LucideUsers class="w-4 h-4" />
-                                <span>{{recipe.servings}} servings</span>
-                            </div>
-                            <Badge variant="outline" class="text-xs">
-                                {{preference?.skillLevel}}
-                            </Badge>
-                        </div>
-                        <div class="flex flex-wrap gap-1 mb-3">
-                            <Badge v-for="tag in recipe.tags" :key="tag" variant="secondary" class="text-xs bg-emerald-100 text-emerald-700 dark:bg-emerald-700 dark:text-emerald-100">
-                                {{tag}}
-                            </Badge>
-                        </div>
-                        <div class="text-xs text-foreground/85">You have: {{preference?.ingredients}}</div>
-                    </CardContent>
-                </Card>
+                <RecipeCard v-for="recipe in recipes" :key="recipe.name" :recipe />
             </div>
         </div>
     </div>
