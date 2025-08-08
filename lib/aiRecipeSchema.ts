90537d1e2d77b8ed79ac4bf55f7006e956df1ef0
Andualem <andualem.fereja12@gmail.com>
2025-08-08 17:57:44 +0300

    chore: create a zod schema for ai generated recipes

---
+++ lib/aiRecipeSchema.ts
@@ -0,0 +1,10 @@
+import * as z from 'zod'
+
+export const aiRecipeSchema = z.object({
+    name: z.string(),
+    tags: z.array(z.string()),
+    cookTime: z.string().transform(value => [Number(value)]),
+    ingredients: z.array(z.string()),
+    instructions: z.array(z.string()),
+    servings: z.string().transform(value => Number(value))
+})
