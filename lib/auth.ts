8790db8a7b949bc75df0fb58634bf816433cc022
Andualem <andualem.fereja12@gmail.com>
2025-08-08 17:54:55 +0300

    feat: add social provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,18 @@ import { betterAuth } from "better-auth"
 import { MongoClient } from "mongodb"
 import { mongodbAdapter } from "better-auth/adapters/mongodb"
 
-const client = new MongoClient(useRuntimeConfig().mongoose.uri)
+const config = useRuntimeConfig()
+const client = new MongoClient(config.mongoose.uri)
 const db = client.db()
 export const auth = betterAuth({
     database: mongodbAdapter(db),
     emailAndPassword: {
         enabled: true
-    }
+    },
+    socialProviders: {
+        google: {
+            clientId: config.GOOGLE_CLIENT_ID as string,
+            clientSecret: config.GOOGLE_CLIENT_SECRET as string
+        }
+    } 
 })
