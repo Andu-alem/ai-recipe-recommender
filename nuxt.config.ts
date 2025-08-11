3164f7af5f59aff0a19412448870081ae96ed400
Andualem <andualem.fereja12@gmail.com>
2025-08-11 11:56:15 +0300

    chore: add sentry monitoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+
   app: {
     head: {
       title: 'ChefGPT - Cooking Companion',
@@ -18,12 +19,14 @@ export default defineNuxtConfig({
       ],
     },
   },
+
   runtimeConfig: {
     dbUri: process.env.MONGODB_URI,
     GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
   },
+
   modules: [
     '@nuxt/eslint',
     'nuxt-mongoose',
@@ -34,20 +37,25 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'nuxt-lucide-icons',
     '@nuxtjs/color-mode',
-    'motion-v/nuxt'
+    'motion-v/nuxt',
+    '@sentry/nuxt/module'
   ],
+
   mongoose: {
     uri: process.env.MONGODB_URI,
     options: {},
     modelsDir: 'models',
     devtools: true
   },
+
   css: ['~/assets/css/tailwind.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -59,7 +67,21 @@ export default defineNuxtConfig({
      */
     componentDir: './components/ui'
   },
+
   colorMode: {
     classSuffix: ''
+  },
+
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: 'andudev',
+      project: 'ai-recipe-recommender'
+    },
+
+    autoInjectServerSentry: 'top-level-import'
+  },
+
+  sourcemap: {
+    client: 'hidden'
   }
 })
---
+++ sentry.client.config.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/nuxt";
+
+Sentry.init({
+  // If set up, you can use your runtime config here
+  // dsn: useRuntimeConfig().public.sentry.dsn,
+  dsn: "https://e21c85c5081ba0a7fdfc677df1344219@o4509771605213184.ingest.de.sentry.io/4509771827839056",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
