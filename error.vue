e522ccd707b07c4c4faaacc83ddf9d7a62404ed3
Andualem <andualem.fereja12@gmail.com>
2025-08-14 00:48:50 +0300

    fix: update alignment

--- error.vue
+++ error.vue
@@ -10,12 +10,14 @@ defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center gap-4">
-        <h1 class="text-amber-500 text-4xl">{{ error?.statusCode }}</h1>
-        <h1>{{ error?.message }}</h1>
-        <p class="text-center">There was an error 😱</p>
-        <Button variant="ghost" as-child>
-            <NuxtLink to="/">Back to Home</NuxtLink>
-        </Button>
+    <div class="h-[100vh] w-full flex items-center justify-center">
+        <div class="flex flex-col items-center justify-center gap-4">
+            <h1 class="text-amber-500 text-4xl">{{ error?.statusCode }}</h1>
+            <h1>{{ error?.message }}</h1>
+            <p class="text-center">There was an error 😱</p>
+            <Button variant="ghost" as-child>
+                <NuxtLink to="/">Back to Home</NuxtLink>
+            </Button>
+        </div>
     </div>
 </template>
