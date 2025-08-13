60bc72d158057ce880eeea6466e128cb32ef09a7
Andualem <andualem.fereja12@gmail.com>
2025-08-13 23:21:33 +0300

    chore: add upstash package and create config for envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,9 @@ export default defineNuxtConfig({
     dbUri: process.env.MONGODB_URI,
     GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN
   },
 
   modules: [
